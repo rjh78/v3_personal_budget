23f1ed5abf5bf95222c89f77c5e8abbe20f2937f
RJH78 <35325011+rjh78@users.noreply.github.com>
2024-05-24 19:19:52 -0400

    includes .sql file in .gitignore, not sure what changes in queries.js

--- queries.js
+++ queries.js
@@ -13,7 +13,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB.PORT,
+  port: process.env.DB_PORT,
 });
 
 /*
